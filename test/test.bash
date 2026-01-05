6df2e804ed8842ab167c5da06dadf8d8839445e2
peanutnuts <ibuki.2.haga@gmail.com>
2026-01-05 23:33:53 +0900

    edit

--- test/test.bash
+++ test/test.bash
@@ -1,5 +1,5 @@
 #/bin/bash
-
+"""
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -10,3 +10,23 @@ timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 1Listen: 10'
+"""
+
+set -e
+
+source /opt/ros/humble/setup.bash
+source ~/ros2_ws/install/setup.bash
+
+echo "Start deg_to_rad node"
+ros2 run mypkg degr2rad &
+NODE_PID=$!
+
+sleep 2
+
+echo "Publish 30 deg"
+ros2 topic pub --once /angle_deg std_msgs/msg/Float64 "{data: 30.0}"
+
+echo "Subscribe result"
+ros2 topic echo /angle_rad --once
+
+kill $NODE_PID
