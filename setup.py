49d3753b6b88e2ff2538ee4d4af32de4b515418d
peanutnuts <ibuki.2.haga@gmail.com>
2026-01-07 02:18:37 +0900

    add SPDX to setup.cfg setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2026 Ibuki Haga
+# SPDX-License-Identifier: BSD-3-Clause
+
 from setuptools import find_packages, setup
 import os
 from glob import glob
