b996cbccea573721fa914d5506ab4d37cc7e2431
peanutnuts <ibuki.2.haga@gmail.com>
2026-01-06 15:09:02 +0900

    edit test.bash

--- test/test.bash
+++ test/test.bash
@@ -23,7 +23,7 @@ ros2 topic pub --once /angle_deg std_msgs/msg/Float64 "{data: 30.0}" --qos-relia
 sleep 2
 
 echo "===== Check output ====="
-OUTPUT=$(timeout 5 ros2 topic echo /angle_rad --once --qos-reliability reliable)
+OUTPUT=$(ros2 topic echo /angle_rad)
 
 echo "OUTPUT:"
 echo "$OUTPUT"
